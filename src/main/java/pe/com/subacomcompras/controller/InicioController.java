0704ef33aa716cfcf71865cfb809386c03d2be54
Piero Batti <piero.9825@gmail.com>
2023-11-17 22:49:11 -0500

    xd

--- src/main/java/pe/com/subacomcompras/controller/InicioController.java
+++ src/main/java/pe/com/subacomcompras/controller/InicioController.java
@@ -5,9 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class InicioController {
-    
-   @GetMapping
-   public String MostrarInicio(){
+        @GetMapping
+        public String MostrarInicio(){
         return "index";
         }
 }
